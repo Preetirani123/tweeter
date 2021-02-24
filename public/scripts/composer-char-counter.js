f08d133fab9bf5877d8fd40d8ffb7e213ae70dd6
Preetirani123 <rani.preeti3333@gmail.com>
2021-02-24 01:47:38 +0000

    created the charactor counter dynamic/ creating the tweet page and fix the nav section

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+  $("#tweet-text").on('keyup keypress blur change' ,function(event) {
+    $text = $(this).val();
+    $charsLeft = 140 - $text.length;
+    $counter = $(this).closest("form").find(".counter")
+    $counter.text($charsLeft);
+
+    // Remaining text is in negative then add the class name
+    if ($charsLeft < 0) {
+      $counter.addClass("redColor");
+    } else {
+      $counter.removeClass("redColor");
+    }
+
+  });
+});
